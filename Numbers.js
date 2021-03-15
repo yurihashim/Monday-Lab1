dfafc60ad35d9161f9792edb5154c667e8298325
yurihashim <yurihashim@icloud.com>
2021-03-15 10:56:01 -0700

    first commit

---
+++ Numbers.js
@@ -0,0 +1,71 @@
+// Implement the following functions
+
+const stringLib = () => 
+    {
+
+function add(a, b) {
+    // add a to b
+  }
+
+function sub(a, b) {
+    // sub a from b
+  }
+
+function mul(a, b) {
+    // mul a by b
+  }
+
+function div(a, b) {
+    // dib a by b
+  }
+
+function Exponentiation(a, b) {
+    // expont a of b
+  }
+
+function Increment(a, b) {
+    // inc a by b
+  }
+
+  
+function Decrement(a, b) {
+    // dec a by b
+  }
+  
+function NumberListA(start,end)
+    {
+    // return list of numbers starting at start and ending at end
+    }
+
+function NumberListB(start,count, skipEvery)
+    {
+    // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
+    }
+
+function NumberListC(start,count, shouldBeRandomOrNot)
+    {
+    // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
+    }
+
+function IsOdd(aNumber);
+    {
+    // is a number odd? true false
+    }        
+
+function IsEven(aNumber);
+    {
+    // is a number even? true false
+    }        
+
+function RandomNumberList(start,end);
+    {
+     // return list of random numbers between start and end
+    }        
+
+function RandomNumberList(start,end,count, allowDuplicates)
+    {
+    // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
+    }        
+
+
+    }
