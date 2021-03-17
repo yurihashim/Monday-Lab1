2254b2f27ad9e6accd7eb0ea8f0ec694713d7f31
yurihashim <yurihashim@icloud.com>
2021-03-17 08:35:20 -0700

    upload files

--- Numbers.js
+++ Numbers.js
@@ -1,71 +1,114 @@
-// Implement the following functions
+// //Implement the following functions
 
-const stringLib = () => 
-    {
+// // const stringLib = () => 
+// //    {
 
-function add(a, b) {
-    // add a to b
-  }
+// function add(a, b) {
+//     return a + b; 
+//   }
+//   console.log (add(1,2));   
 
-function sub(a, b) {
-    // sub a from b
-  }
+// function sub(a, b) {
+//     return a - b;  
+//   }
+//   console.log (sub (2,1)); 
 
-function mul(a, b) {
-    // mul a by b
-  }
+// function mul(a, b) {
+//     return a * b; 
+//   }
+//   console.log (mul (1,2)); 
 
-function div(a, b) {
-    // dib a by b
-  }
+// function div(a, b) {
+//     return a / b; 
+//   }
+//   console.log (div (9,3)); 
 
-function Exponentiation(a, b) {
-    // expont a of b
-  }
+// function Exponentiation(a, b) {
+//     return a ** b; 
+//   }
+//   console.log (Exponentiation (2,3)); 
 
-function Increment(a, b) {
-    // inc a by b
-  }
+// function Increment(a,b) {
+//     return add (a,b)
+//   }
+//   console.log(Increment(2,1)); 
+
+// function Decrement(a, b) {
+//     return sub(a,b); 
+//   }
+//   console.log(Decrement(2,1)); 
+  
+// function NumberListA(start,end){
+//     // return list of numbers starting at start and ending at end
+// let someList = []; // make an empty arry
+// for (let count = start; count <= end; count ++) // begin the loop
+// {
+//     someList.push(count); //add to the array, the count (which is the number in the loop)
+// }
+// return someList; 
+//     }
+
+// function NumberListB(start,count, skipEvery)
+//     {
+//   // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
+//   let someList = []; // empty array
+//   let _skipCount = 0; // sert this to 0 initially, we use it to keep track of when we should skip it or not
+//   for (let aNumber = start ; (aNumber <= (start+ count)); aNumber++) //begin the loop
+//   {
+//   if (_skipCount == skipEvery)
+//   {
+//   someList.push (aNumber); 
+//   _skipCount = 0;
+//   }
+//   _skipCount++; 
+//   if (_skipCount >= skipEvery){_skipCount = 0; }
+//   }
+//      return someList; 
+//     }
+
+// function NumberListC(start,count, shouldBeRandomOrNot)
+//   {
+//     // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
+//     let someList = [];
+//     for (let bNumber = start; (bNumber < (start + count)); bNumber++)
+//     {
+//       if (shouldBeRandomOrNot === true){
+//       someList.push(RandomNumber (start, (start + count))); 
+//     }
+//     else
+//     {
+//       someList.push (bNumber); 
+//     }
+//     return someList; 
+//   }
+
+// function IsOdd(aNumber)
+//     {
+//     if (aNumber % 2 == 0)
+//     return true; 
+//     else 
+//     return false; 
+//     }      
+//     console.log(IsOdd(2));   
 
   
-function Decrement(a, b) {
-    // dec a by b
-  }
-  
-function NumberListA(start,end)
-    {
-    // return list of numbers starting at start and ending at end
-    }
+// function IsEven(aNumber)
+//     {
+//     if (aNumber % 2 == 0)
+//     return true; 
+//     else 
+//     return false; 
+//     }      
+//     console.log(IsEven(10));  
+ 
 
-function NumberListB(start,count, skipEvery)
-    {
-    // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
-    }
+// function RandomNumberList(start,end)
+//     {
+//     //return list of random numbers between start and end
+//     }        
 
-function NumberListC(start,count, shouldBeRandomOrNot)
-    {
-    // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
-    }
-
-function IsOdd(aNumber);
-    {
-    // is a number odd? true false
-    }        
-
-function IsEven(aNumber);
-    {
-    // is a number even? true false
-    }        
-
-function RandomNumberList(start,end);
-    {
-     // return list of random numbers between start and end
-    }        
-
-function RandomNumberList(start,end,count, allowDuplicates)
-    {
-    // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
-    }        
-
-
-    }
+// function RandomNumberList(start,end,count, allowDuplicates)
+//     {
+//     // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
+//    }
+// }   
--- strings.js
+++ strings.js
@@ -1,54 +1,85 @@
 // Implement the following functions
 // your code. Refactor this file as you see fit. Export or modify any function as you see fit. Add additonal functions as you see fit. 
 
-const stringLib = () => 
+//const stringLib = () => 
+//    {
+
+// Append two strings together, by taking the first string, calling concatanate on it, and giving it the value b
+// function concatenate(a,b)
+// {
+//     let a = "Yuri"; 
+//     let b = "Hashimoto"; 
+//     let result = a.concat(b); 
+//     return result; 
+// }
+
+function GetCharsFromTable (startAt, endAt)
+{
+    let aList = []; 
+    for (var i = startAt; startAt < endAt; i++)
     {
-
-function concatanate(a,b)
-{
-// append one string to another
+        aList.push(String.fromCharCode(i)); 
+    }
+    return aList; 
 }
 
 
-function ASCIINumberToChar(Number)
+//take one number, say, 127 and convert it to its ASCII char representation
+function ASCIINumberToChar(number)
 {
-// take one number, say, 127 and convert it to its ASCII char representation
+  return number.GetCharsFromTable(); 
 }
 
+
+// take one Character, say, 'A' and convert it to it's Number representation
 function CharToAsciiNumber(aChar)
 {
-// take one Character, say, 'A' and convert it to it's Number representation
+    return aChar.GetCharsFromTable (); 
 }
 
-function ReverseString(origionalString)
-{
-// return a string, ie: Hello becomes olleH
-}
+    // return a string, ie: Hello becomes olleH
+// function ReverseString(origionalString)
+// {
+//     return origionalString.split("").reverse().join(""); 
+// }
+// console.log (ReverseString ("Hello")); 
 
-function GenerateOnlyUpperCase()
-{
-//Ie: A,B,C,D,E,F,G...Z
-}
+// function GenerateOnlyUpperCase(string)
+// {
+//     return string.toUpperCase();  
+// }
+// console.log (GenerateOnlyUpperCase ('Hello')); 
 
-function GenerateOnlyLowerCase()
-{
-//Ie: a,b,c,d,e,f,g...z
-}
 
+// function GenerateOnlyLowerCase(string)
+// {
+//     return string.toLowerCase(); 
+// }
+// console.log (GenerateOnlyLowerCase('HELLO')); 
+
+
+// Make a loop, and show only numbers, 0 to 9 and return that in a list
 function GenerateOnlyNumbers()
 {
-//Ie: 0,1,2,3...9
+    let arr =  []
+    While (arr.length < 9)
+    {
+        let r = Math.floor (Math.random() * 9) +1; 
+        if (arr.indexOf(r) === -1) arr.push(r); 
+    }
 }
+console.log (arr('9,10,20,0,1,4,5')); 
 
-function GenerateOnlySymbols()
-{
-//Ie: $, %, !, #, etc
-}
+// function GenerateOnlySymbols()
+// {
+// //Ie: $, %, !, #, etc
+// }
 
 
-function GenerateUpperAndLowerCase()
-{
-    //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
-}
+// function GenerateUpperAndLowerCase()
+// {
+//     //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
+// }
 
-}
+// }
+
